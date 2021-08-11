f64abb6379cc2ed681589a18520aab73264dfada
sindhuja1999 <gampasindhuja999@gmail.com>
2021-08-11 13:31:18 +0530

    1 file modified

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -2,7 +2,11 @@ import { User } from '../interfaces'
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
-  { id: 1, name: 'Alice' },
+  { id: 1, name: 'Alice',
+  keyword: 'Blockchain',
+  goal: 'technology',
+  matches:18,
+  search_status:'Done' },
   { id: 102, name: 'Bob' },
   { id: 103, name: 'Caroline' },
   { id: 104, name: 'Dave' },
